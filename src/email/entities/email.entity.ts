79a3c4f46f3caf3d2a5ae4c26bb4625772a5db0d
Leonardorossato <leonardorossato21@gmail.com>
2023-02-25 10:44:58 -0300

    update email.entity.ts only typeorm

--- src/email/entities/email.entity.ts
+++ src/email/entities/email.entity.ts
@@ -1,7 +1,12 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
-
-@ObjectType()
+import { PrimaryGeneratedColumn, Entity, Column } from 'typeorm';
+@Entity()
 export class Email {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false, type: 'varchar', length: 255 })
+  nome: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 255, unique: true })
+  email: string;
 }
