0653e141da83dee2943e56130acb025f64a7dc88
Leonardorossato <leonardorossato21@gmail.com>
2023-02-25 10:44:48 -0300

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { EmailModule } from './email/email.module';
 
 
 @Module({
-  imports: [EmailModule]
+  imports: [EmailModule, ConfigModule.forRoot({isGlobal: true})]
 })
 export class AppModule {}
