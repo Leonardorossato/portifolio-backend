848c0730e86b2512070031739f8dcdf5e30d08e4
Leonardorossato <leonardorossato21@gmail.com>
2023-02-26 10:51:48 -0300

    update ormconfig.ts

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -1,21 +1,21 @@
 import {
   TypeOrmModuleAsyncOptions,
-  TypeOrmModuleOptions,
+  TypeOrmModuleOptions
 } from '@nestjs/typeorm';
-import { Email } from '../email/entities/email.entity';
+import { Email } from 'src/email/entities/email.entity';
 
 export const PostgresSqlConnection: TypeOrmModuleAsyncOptions = {
   useFactory: async (): Promise<TypeOrmModuleOptions> => {
     return {
       type: 'postgres',
-      port: process.env.DB_PORT,
+      port: +process.env.DB_PORT,
       host: process.env.DB_HOST,
       database: process.env.DB_DATABASE,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       entities: [Email],
-      migrations: [__dirname + '**/migrations/**{.ts,.js}'],
-      synchronize: true,
+      migrations: [__dirname + '/**/migrations/**{.ts,.js}'],
+      synchronize: false,
       migrationsRun: true,
     };
   },
