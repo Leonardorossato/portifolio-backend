b2495576f02e3a874a2e4810dacb2e3c46b05c27
Leonardorossato <leonardorossato21@gmail.com>
2023-02-26 10:51:40 -0300

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,8 @@
+import { ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloDriver } from '@nestjs/apollo/dist/drivers';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql/dist/graphql.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostgresSqlConnection } from './config/ormconfig';
 import { EmailModule } from './email/email.module';
@@ -7,8 +10,13 @@ import { EmailModule } from './email/email.module';
 @Module({
   imports: [
     EmailModule,
-    ConfigModule.forRoot({isGlobal: true}),
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync(PostgresSqlConnection),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: true
+    })
   ],
 })
 export class AppModule {}
