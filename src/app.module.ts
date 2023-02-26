441fff71b9d6ed3a05dd217922f25563a0461cab
Leonardorossato <leonardorossato21@gmail.com>
2023-02-26 09:11:25 -0300

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresSqlConnection } from './config/ormconfig';
 import { EmailModule } from './email/email.module';
 
-
 @Module({
-  imports: [EmailModule, ConfigModule.forRoot({isGlobal: true})]
+  imports: [
+    EmailModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(PostgresSqlConnection),
+  ],
 })
 export class AppModule {}
