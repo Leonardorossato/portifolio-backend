fe8c39041c611dcfe0bfc9fbd8f37bcc7e765039
Leonardorossato <leonardorossato21@gmail.com>
2023-02-26 09:00:06 -0300

    update email.resolver.ts

--- src/email/email.resolver.ts
+++ src/email/email.resolver.ts
@@ -1,35 +1,18 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
-import { EmailService } from './email.service';
-import { Email } from './entities/email.entity';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateEmailInput } from './dto/create-email.input';
-import { UpdateEmailInput } from './dto/update-email.input';
+import { EmailService } from './email.service';
 
-@Resolver(() => Email)
+@Resolver(() => EmailService)
 export class EmailResolver {
   constructor(private readonly emailService: EmailService) {}
 
-  @Mutation(() => Email)
+  @Mutation(() => EmailService)
   createEmail(@Args('createEmailInput') createEmailInput: CreateEmailInput) {
     return this.emailService.create(createEmailInput);
   }
 
-  @Query(() => [Email], { name: 'email' })
+  @Query(() => [EmailService], { name: 'email' })
   findAll() {
     return this.emailService.findAll();
-  }
-
-  @Query(() => Email, { name: 'email' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.emailService.findOne(id);
-  }
-
-  @Mutation(() => Email)
-  updateEmail(@Args('updateEmailInput') updateEmailInput: UpdateEmailInput) {
-    return this.emailService.update(updateEmailInput.id, updateEmailInput);
-  }
-
-  @Mutation(() => Email)
-  removeEmail(@Args('id', { type: () => Int }) id: number) {
-    return this.emailService.remove(id);
   }
 }
